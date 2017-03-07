6377cf33ffc432af2c4b739b7299778ccf40299a
"Cayle <"graumannc@missouri.edu">
2017-03-07 12:23:36 -0600

    Changes to SQL for reportcenter

--- ReportCenter/ReportCenter/sql/01_core/00_fundamentals/910_projexuser.sql
+++
@@ -1,3 +0,0 @@
-EXECUTE CORE.USER('PROJEX4', null, null, null, 1, null, null, 0, 'SYSTEM', 'INVITED', 1, null, null, 0, null);
-
-COMMIT;
--- ReportCenter/ReportCenter/sql/01_core/99_data/04_user_list_data.sql
+++
@@ -1,5 +0,0 @@
-EXECUTE CORE.LIST('user.usertype','User Types',null,1,1);
-EXECUTE CORE.LISTITEM('user.usertype',null,1,2,'ENTERPRISE','ENTERPRISE',0,'Enterprise User');
-
-COMMIT;
-
